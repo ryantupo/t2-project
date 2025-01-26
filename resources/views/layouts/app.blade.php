6678b51d551fb18362a9f9ed01c725e21a89c749
Ryan Tuplin <ryan.tuplin@bodis.com>
2025-01-26 21:36:42 +0000

    icon change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
     <!-- Slick Carousel JS -->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
+    <link rel="icon" href="{{ asset('images/logo_cropped.png') }}" type="image/png">
 
 
     @vite('resources/css/app.css')
